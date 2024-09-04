e09f8e283518f2a50532218d3b0e1b7859e171fb
Tonny Bett <betttonny26@gmail.com>
2024-09-04 10:08:27 +0300

    made changes to print the code

--- lib/python.py
+++ lib/python.py
@@ -1,19 +1,17 @@
 #Function: add_numbers
 def add_numbers(num1, num2):
     return num1 + num2
-
+print(add_numbers(1, 2))
 #Function: is_even 
 def is_even(number):
     return number % 2 == 0
+print(is_even(2))
 
 #Function: reverse_string
 def count_vowels(text):
     vowels = 'aeiouAEIOU'
     return sum(1 for char in text if char in vowels)
-
-def count_vowels(text):
-    vowels = 'aeiouAEIOU'
-    return sum(1 for char in text if char in vowels)
+print(count_vowels("hello"))
 
 #Function: calculate_factorial
 def calculate_factorial(n):
@@ -23,6 +21,7 @@ def calculate_factorial(n):
     for i in range(1, n + 1):
         result *= i
     return result
+print(calculate_factorial(5))
 
 #Function: decorator_func
 def decorator_func(func):
@@ -30,13 +29,14 @@ def decorator_func(func):
         print("Decorator Applied")
         return func(*args, **kwargs)
     return wrapper
-
 def apply_decorator(func):
     return decorator_func(func)
+print(apply_decorator(lambda x: x + 1)(2))
 
 #Sequences: Sort List of Tuples 
 def sort_by_age(list_of_tuples):
     return sorted(list_of_tuples, key=lambda x: x[1])
+print(sort_by_age([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
 
 def merge_dicts(dict1, dict2):
     merged_dict = dict1.copy()
@@ -46,6 +46,7 @@ def merge_dicts(dict1, dict2):
         else:
             merged_dict[key] = value
     return merged_dict
+print(merge_dicts({1: 2, 3: 4}, {2: 3, 4: 5}))
 
 #Object-Oriented Programming: Class Creation 
 class Car:
@@ -53,6 +54,6 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
-
     def display_info(self):
         print(f"Car Make: {self.make}, Model: {self.model}, Year: {self.year}")
+print(Car("Toyota", "Camry", 2022).display_info())
